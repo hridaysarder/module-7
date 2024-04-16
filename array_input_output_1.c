7bfff4584972dc9783a9ecc24315f8e47722cd2e
HridaySarder <hridoyaug12@gmail.com>
2024-04-16 23:34:26 +0600

    first commit

---
+++ array_input_output_1.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+{
+    int arr[3];
+    scanf("%d %d %d",&arr[0],&arr[1],&arr[2]);
+    // arr[0]=200;
+    // arr[1]=300;
+    // arr[2]=400;
+    printf("%d %d %d",arr[0],arr[1],arr[2]);
+    return 0;
+}
